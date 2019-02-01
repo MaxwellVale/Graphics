4ec827648c345e225beb79d2774177ff682973f5
Mackswehl <max.s.vale01@gmail.com>
2019-02-01 07:53:29 -0500

    Smol changes

--- Work0/picmaker.py
+++ Work0/picmaker.py
@@ -6,7 +6,8 @@ for x in range(600):
     for y in range(600):
         r = 255
         b = int(x / 600. * 255)
-        g = int(y / 600. * 255)
+        g = 255 - b
+
         file.write(str(r) + ' ' + str(b) + ' ' + str(g) + ' ')
 
 file.close()
