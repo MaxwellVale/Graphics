71b9f50680303486a8cfb0ea2a45b9a8cdfea466
Mackswehl <max.s.vale01@gmail.com>
2019-02-01 07:18:48 -0500

    Add hw0 file

---
+++ Work0/picmaker.py
@@ -0,0 +1,12 @@
+file = open("newImage.ppm", 'w')
+
+file.write("P3 600 600 255 \n")
+
+for x in range(600):
+    for y in range(600):
+        r = 255
+        b = int(x / 600. * 255)
+        g = int(y / 600. * 255)
+        file.write(str(r) + ' ' + str(b) + ' ' + str(g) + ' ')
+
+file.close()
