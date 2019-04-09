2277e120d368ed0c680a0ff201ca547360008dd2
Mackswehl <max.s.vale01@gmail.com>
2019-04-09 10:41:57 -0400

    Gallery submission stuff

--- Work07/main.py
+++ Work07/main.py
@@ -12,4 +12,15 @@ t = new_matrix()
 ident(t)
 csystems = [ t ]
 
-parse_file( 'script', edges, polygons, csystems, screen, color )
+# parse_file( 'newScript', edges, polygons, csystems, screen, color )
+c = 50
+r1 = 10
+r2 = 20
+num = 0
+while num < 50:
+    add_torus(polygons, c, c, c, r1, r2, 20)
+    c += 10
+    num += 1
+draw_polygons(polygons, screen, color)
+display(screen)
+save_extension(screen, "moreGal.png")
