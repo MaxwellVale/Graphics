d365b1506b09af80a5ac1684d085aa1efb5a460a
Mackswehl <max.s.vale01@gmail.com>
2019-05-06 09:11:38 -0400

    Homework 9 Lighting files

--- Work09/gmath.py
+++ Work09/gmath.py
@@ -22,19 +22,54 @@ SPECULAR_EXP = 4
 
 #lighting functions
 def get_lighting(normal, view, ambient, light, areflect, dreflect, sreflect ):
-    pass
+    a = calculate_ambient(ambient,areflect)
+    d = calculate_diffuse(light,dreflect,normal)
+    s = calculate_specular(light,sreflect,view,normal)
 
+    return [ limit_color(int(a[0] + d[0] + s[0])),
+             limit_color(int(a[1] + d[1] + s[1])),
+             limit_color(int(a[2] + d[2] + s[2])),
+           ]
 def calculate_ambient(alight, areflect):
-    pass
+    return [alight[0] * areflect[0],
+             alight[1] * areflect[1],
+             alight[2] * areflect[2]]
+
 
 def calculate_diffuse(light, dreflect, normal):
-    pass
+    normalize(light[0])
+    normalize(normal)
+    return [ limit_color(light[1][0] * dreflect[0] * dot_product(normal,light[0])),
+             limit_color(light[1][1] * dreflect[1] * dot_product(normal,light[0])),
+             limit_color(light[1][2] * dreflect[2] * dot_product(normal,light[0])),
+           ]
 
 def calculate_specular(light, sreflect, view, normal):
-    pass
+    normalize(light[0])
+    normalize(normal)
+    normalize(view)
+    dp = 2 * dot_product(light[0],normal)
+    dir = [ normal[0] * dp - light[0][0],
+                       normal[1] * dp - light[0][1],
+                       normal[2] * dp - light[0][2],
+                     ]
+    d = dot_product(dir,view)
+    if d < 0:
+      d = 0
+
+    return [ limit_color(light[1][0] * sreflect[0] * d),
+             limit_color(light[1][1] * sreflect[1] * d),
+             limit_color(light[1][2] * sreflect[2] * d),
+           ]
 
 def limit_color(color):
-    pass
+    if color < 0:
+      return 0
+    elif color > 255:
+      return 255
+    else:
+      return color
+
 
 #vector functions
 #normalize vetor, should modify the parameter
